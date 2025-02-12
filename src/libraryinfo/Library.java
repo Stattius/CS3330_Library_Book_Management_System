742df4e1afb41b17faa7c050920a51f8ebdfaef2
Stattius <xxblade6475xx@gmail.com>
2025-02-11 22:24:41 -0600

    the Library construct is made to handle both books array and count of books

--- src/libraryinfo/Library.java
+++ src/libraryinfo/Library.java
@@ -1,5 +1,20 @@
 package libraryinfo;
 
 public class Library {
-	//comment
+	
+		//attributes
+	private Book[] books;
+	private int count;
+
+		//Library construct to set values
+	public Library() {
+		this.books = new Book[5];
+		this.count = 0;
+	}
+	
+	
+	
+	
+	
+	
 }
