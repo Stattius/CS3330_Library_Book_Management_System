abeebcad3ea50d5c889ace8ad3143743c21ce8d0
LOTSWA <55159891+LOTSWA@users.noreply.github.com>
2025-02-12 15:31:31 -0600

    Added NewBook method and Book Constructor

--- src/libraryhome/LibraryApp.java
+++ src/libraryhome/LibraryApp.java
@@ -1,11 +1,15 @@
 package libraryhome;
 
+import libraryinfo.Book;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test statement");
 		System.out.println("test test test");
+		Book.NewBook(null);
+		//Book.SetBook("Return of the king", "Tolkien", "123abc", , 0);
 	}
 
 }
--- src/libraryinfo/Book.java
+++ src/libraryinfo/Book.java
@@ -1,9 +1,37 @@
 package libraryinfo;
 
 public class Book {
-	String title = "_";
-	String author = "_";
+	String Title = "_";
+	String Author = "_";
 	String ISBN = "_";
-	double price = 0.0;
+	double Price = 0.0;
 	//test test
+
+
+	public Book() {
+		String Set ="Unknown";
+		Title = Set;
+		Author = Set;
+		ISBN = Set;
+		Price = 0.0;
+	}
+	
+	
+	public static void NewBook(String[] args) {
+		
+		Book rotk = new Book();
+		System.out.println(rotk.Author + "\n" + rotk.Title);
+		
+	}
+	
+	public static void SetBook(Book BookName, String Title, String Author, String ISBN, double Price) {
+		
+		BookName.Title = Title;
+		BookName.Author = Author;
+		BookName.ISBN = ISBN;
+		BookName.Price = Price;
+		
+		System.out.println(BookName.Author + "\n" + BookName.Title);
+		
+	}	
 }
