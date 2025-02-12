4145af1658d99075a3e4535acc4806230d9f4e6a
Stattius <xxblade6475xx@gmail.com>
2025-02-12 15:02:09 -0600

    methods addbook and searchByISBN tests from main are implemented

--- src/libraryhome/LibraryApp.java
+++ src/libraryhome/LibraryApp.java
@@ -1,5 +1,6 @@
 package libraryhome;
 
+import libraryinfo.Book;
 import libraryinfo.Library;
 
 public class LibraryApp {
@@ -11,10 +12,21 @@ public class LibraryApp {
 	 */
 	Library library = new Library();
 		
+		//adding 3 new books
+	System.out.println("Adding books... ");
+	library.addBook(new Book("Clean Code", "Robert C. Martin", "978-0135166307", 40.99));
+    library.addBook(new Book("Design Patterns", "Erich Gamma", "978-0201633610", 50.99));
+    library.addBook(new Book("The Pragmatic Programmer", "Andrew Hunt", "978-0135957059", 45.99));
 	
 	
-	
-	
+   		//searching for the book by ISBN
+    String search = "978-0135166307";
+    System.out.println("Searching for book with ISBN: " + search);
+    Book found = library.searchByISBN(search);
+    if(found != null) {
+    	System.out.println("Book found: " + found);
+    }
+
 	
 	
 	
