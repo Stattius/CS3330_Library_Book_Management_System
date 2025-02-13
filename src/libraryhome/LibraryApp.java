8ea52812f729f247ba3a98d011320516f4303762
LOTSWA <55159891+LOTSWA@users.noreply.github.com>
2025-02-12 20:20:43 -0600

    merged all files to run together make this the new main blade

--- src/libraryhome/LibraryApp.java
+++ src/libraryhome/LibraryApp.java
@@ -1,32 +1,59 @@
 package libraryhome;
 
 import libraryinfo.Book;
+import libraryinfo.Library;
 
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is a test statement");
+
+	/**
+	 * Library object is created out of Library
+	 */
+	Library library = new Library();
 		
-		
-		Book obj = new Book();
-		obj.bookPrint();
-		obj.setTitle("Return of the King");
-		obj.setAuthor( "Tolkien");
-		obj.setISBN("abc123");
-		obj.setPrice( 50.07);
-		obj.bookPrint();
-		
-		Book obj2 = new Book();
-		obj2.setBook("The Blade itself", "Abercrombie", "efg456", 52.45);
-		obj2.bookPrint();
-		Book copy = new Book(obj);
-		copy.bookPrint();
-		String S1 = copy.toString();
-		System.out.println(copy.toString());
-		//Book.NewBook(null);
-		//Book.SetBook("Return of the king", "Tolkien", "123abc", , 0);
+	/**
+	 * method calls to add a book to the array
+	 */
+	System.out.println("Adding books... ");
+	library.addBook(new Book("Clean Code", "Robert C. Martin", "978-0135166307", 40.99));
+    library.addBook(new Book("Design Patterns", "Erich Gamma", "978-0201633610", 50.99));
+    library.addBook(new Book("The Pragmatic Programmer", "Andrew Hunt", "978-0135957059", 45.99));
+	
+	
+   	/**
+   	 * call to find book based off of ISBN
+   	 */
+    String search = "978-0135166307";
+    System.out.println("Searching for book with ISBN: " + search);
+    Book found = library.searchByISBN(search);
+    if(found != null) {
+    	System.out.println("Book found: " + found);
+    }
+
+   	/**
+   	 * printing off all available books in the array
+   	 */
+    System.out.println("All books in the library:");
+    library.displayBooks();
+    
+    /**
+     * calls for removal of a book from the system
+     */
+    System.out.println("Removing book: Design patterns");
+    library.removeBook(new Book("Design Patterns", "Erich Gamma", "978-0201633610", 50.99));
+	
+    /**
+     * printing off all available books in the array
+     */
+    System.out.println("All books in the library:");
+    library.displayBooks();
+    
+    /**
+     * trying to remove another book that does not exist
+     */
+    System.out.println("Removing book: Design patterns");
+    library.removeBook(new Book("Design Patterns", "Erich Gamma", "978-0201633610", 50.99));
 	}
 
 }
-//test test
--- src/libraryinfo/Library.java
+++ src/libraryinfo/Library.java
@@ -1,5 +1,95 @@
 package libraryinfo;
 
 public class Library {
+	
+	/**
+	* this is the attributes that make up our library
+	* book is set as an array of books
+	*/
+	private Book[] books;
+	private int count;
 
+	/**
+	*this is a Library construct with the values of attributes
+	*books and count, setting the value to 0 and the array
+	*to a maximum of 5
+	*/
+	public Library() {
+		this.books = new Book[5];
+		this.count = 0;
+	}
+	
+	/**
+	* this method will add a book
+	* @param book
+	* 		this argument is the book object thats added
+	* @return
+	* 		return false if the array is full
+	* 		return true if the book was added correctly
+	*/
+	public boolean addBook(Book book) {
+		if (count < books.length) {
+			books[count] = book;
+			count++;
+			System.out.println("Book added successfully");
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	* this method deletes a book from the system
+	* @param book
+	* 		this argument is the book object that is deleted
+	* @return
+	* 		returns true if the book is within the system and can be deleted
+	* 		returns false if the book is not within the system
+	*/
+	public boolean removeBook(Book book) {
+		for(int i = 0; i < count; i++) {
+			if (books[i].equals(book)){
+				books[i] = books[count - 1];
+				books[count - 1] = null;
+				count--;
+				return true;
+			}
+		}
+		System.out.println("Cannot Remove book: " + book + "book does not exist");
+		return false;
+	}
+	
+	
+	/**
+	* this method searches the array of books and
+	* compares ISBN to find the appropriate book
+	* @param ISBN
+	* 		this argument will be compared to other ISBNs in the array
+	* 		and search for it
+	* @return
+	* 		returns the ISBN if it is found
+	* 		returns null if the book is neither within the system or is null value
+	*/
+	public Book searchByISBN(String ISBN) {
+		if(ISBN == null) {
+			System.out.println("Book doesn't exist");
+			return null;
+		}
+		for(int i = 0; i < count; i++) {
+			if (books[i].getISBN().equals(ISBN)) {
+				return books[i];
+			}
+		}
+		System.out.println("Book doesn't exist");
+		return null;
+	}
+	
+	/**
+	* this method loops through the array of books
+	* and displays each book's information
+	*/
+	public void displayBooks() {
+		for(int i = 0; i < count; i++) {
+			System.out.println((i + 1) + ". " + books[i]);
+		}
+	}
 }
